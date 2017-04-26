76a2e7084317f2629a381a3c02304d8709100da2
Scott White <whitews@gmail.com>
2017-04-26 14:02:59 -0400

    add angular cookies to provide Django's csrf token for session auth

--- static/ng-app/app.js
+++ static/ng-app/app.js
@@ -1,8 +1,17 @@
 var app = angular.module(
     'IHCApp',
-    ['ngResource', 'ngRoute', 'ui.bootstrap', 'ngPolyDraw']
+    [
+        'ngResource',
+        'ngRoute',
+        'ngCookies',
+        'ui.bootstrap',
+        'ngPolyDraw'
+    ]
 );
 
+app.config(function($resourceProvider) {
+  $resourceProvider.defaults.stripTrailingSlashes = false;
+});
 
 app.config(function($routeProvider) {
     $routeProvider.when(
@@ -12,4 +21,18 @@ app.config(function($routeProvider) {
             controller: 'ExperimentListController'
         }
     ).otherwise({ redirectTo: '/' });
+});
+
+app.config(function($httpProvider) {
+  $httpProvider.defaults.withCredentials = true;
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $httpProvider.interceptors.push(function($cookies) {
+    return {
+      'request': function(config) {
+        config.headers['X-CSRFToken'] = $cookies.get('csrftoken');
+        return config;
+      }
+    };
+  });
 });
