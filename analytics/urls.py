9ea117e94195a37df04aa96b23b328d69343118e
Scott White <whitews@gmail.com>
2017-05-15 12:37:13 -0400

    added REST API endpoints for Probe model

--- analytics/urls.py
+++ analytics/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     url(r'^api/lungmapexperiments/', api_views.get_lung_map_experiments),
     url(r'^api/experiments/$', api_views.ExperimentList.as_view()),
     url(r'^api/experiments/(?P<experiment_id>[\w{}.-]{1,14})/$', api_views.ExperimentDetail.as_view()),
+    url(r'^api/probes/$', api_views.ProbeList.as_view()),
+    url(r'^api/probes/(?P<pk>[0-9]+)/$', api_views.ProbeDetail.as_view()),
     url(r'^api/images/$', api_views.LungmapImageList.as_view()),
     url(r'^api/images/(?P<pk>[0-9]+)/$', api_views.LungmapImageDetail.as_view()),
     url(r'^api/images-jpeg/(?P<pk>[0-9]+)/$', api_views.get_image_jpeg, name='image-jpeg'),
