267b51c0550b43b6702b10cb9dfc14ec1f685bce
Scott White <whitews@gmail.com>
2017-07-14 05:56:45 -0400

    fix preload script, needed django.setup()

--- preload_analytics_models.py
+++ preload_analytics_models.py
@@ -1,14 +1,15 @@
+import django
+
+django.setup()
+
 from lungmap_client import lungmap_utils
 from analytics import models
 
 image_sets = lungmap_utils.get_image_set_candidates()
 
-# key = 'mus musculus_P07_60X_Acetylated Tubulin__White_CCSP __Red_TTF-1__Green'
-# value = image_sets[key]
-
 for key, value in image_sets.items():
     print('loading imageset ', key)
-    image_set = models.ImageSet.objects.create(
+    image_set = models.ImageSet.objects.get_or_create(
         image_set_name = key,
         magnification = value['magnification'],
         species = value['species'],
@@ -18,38 +19,31 @@ for key, value in image_sets.items():
         experiment, experiment_create = models.Experiment.objects.get_or_create(
             experiment_id = image['image_id'].split('_')[0]
         )
-        imageobject = models.Image.objects.create(
+        imageobject = models.Image.objects.get_or_create(
             s3key = image['s3key'],
             image_name = image['image_name'],
             image_id = image['image_id'],
             x_scaling = image['x_scaling'],
             y_scaling = image['y_scaling'],
-            image_set = image_set,
-            experiment_id = experiment
+            image_set = image_set[0].id,
+            experiment_id = experiment.experiment_id
         )
     for probe in value['probes']:
         probeobject, probeobject_create = models.Probe.objects.get_or_create(
             label = probe['probe_label']
         )
-        imagesetprobemap = models.ImageSetProbeMap.objects.create(
+        imagesetprobemap = models.ImageSetProbeMap.objects.get_or_create(
             color = probe['color'],
             probe_name = probeobject,
-            image_set = image_set
+            image_set = image_set[0].id
         )
-        #TODO: find a way to load experimentprobemap, errors currently thrown
-        # for exp in value['experiments']:
-        #     experimentobject = models.Experiment.objects.get(
-        #         experiment_id = exp
-        #     )
-            # experimentprobemap = models.ExperimentProbeMap.objects.create(
-            #     color = probe['color'],
-            #     experiment_id = experimentobject,
-            #     probe_name=probeobject,
-            # )
 
-
-
-#Load Experiments
-
-
-#Load Probes
+        for exp in value['experiments']:
+            experimentobject = models.Experiment.objects.get(
+                experiment_id = exp
+            )
+            experimentprobemap = models.ExperimentProbeMap.objects.create(
+                color = probe['color'],
+                experiment_id = experimentobject,
+                probe_name=probeobject,
+            )
