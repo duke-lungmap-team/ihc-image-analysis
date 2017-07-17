4c71a8f109b260fb8c18d4d99324b7b513f79b4c
benneely <nigelneely@gmail.com>
2017-07-17 12:36:07 -0400

    removing additionally unused code in api_views, also making the posting to /image/{pk} atomic

--- analytics/api_views.py
+++ analytics/api_views.py
@@ -8,7 +8,6 @@ from rest_framework import generics, permissions, status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from tqdm import tqdm
 import django_filters
 
 
@@ -38,70 +37,6 @@ class ImageSetDetail(generics.RetrieveAPIView):
     serializer_class = serializers.ImageSetDetailSerializer
 
 
-class ExperimentList(generics.ListCreateAPIView):
-    """
-    List all experiments, or create a new experiment.
-    """
-
-    queryset = models.Experiment.objects.all()
-    serializer_class = serializers.ExperimentSerializer
-
-    def post(self, request, *args, **kwargs):
-        data = request.data
-
-        try:
-            with transaction.atomic():
-                exp = models.Experiment(
-                    experiment_id=data['experiment_id']
-                )
-                exp.save()
-
-                images = lungmap_utils.get_images_by_experiment(exp.experiment_id)
-                exp_probes = lungmap_utils.get_probes_by_experiment(exp.experiment_id)
-
-                for exp_probe in tqdm(exp_probes):
-                    probe, created = models.Probe.objects.get_or_create(
-                        label=str.strip(exp_probe['probe_label'])
-                    )
-
-                    models.ExperimentProbeMap(
-                        probe=probe,
-                        color=str.strip(exp_probe['color']).lower(),
-                        experiment_id=exp
-                    ).save()
-
-                for image in tqdm(images):
-                    suf, sha1, suf_jpeg = lungmap_utils.get_image_from_s3(image['s3key'])
-                    models.Image(
-                        s3key=image['s3key'],
-                        magnification=image['magnification'],
-                        image_name=image['image_name'],
-                        experiment=exp,
-                        image_id=image['image_id'],
-                        x_scaling=image['x_scaling'],
-                        y_scaling=image['y_scaling'],
-                        image_orig=suf,
-                        image_orig_sha1=sha1,
-                        image_jpeg=suf_jpeg
-                    ).save()
-        except Exception as e:  # catch any exception to rollback changes
-            if hasattr(e, 'messages'):
-                return Response(data={'detail': e.messages}, status=400)
-            return Response(data={'detail': e.message}, status=400)
-
-        serializer = serializers.ExperimentSerializer(
-            exp,
-            context={'request': request}
-        )
-        headers = self.get_success_headers(serializer.data)
-
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED,
-            headers=headers
-        )
-
-
 class ImageDetailView(APIView):
     """
     A view to get specific image metadata and to post to cache images
@@ -113,18 +48,26 @@ class ImageDetailView(APIView):
             serializer.data,
             status=status.HTTP_200_OK
         )
+
     def post(self, request, *args, **kwargs):
-        img = get_object_or_404(models.Image, id=kwargs['pk'])
-        suf, sha1, suf_jpeg = lungmap_utils.get_image_from_s3(img.s3key)
-        img.image_orig = suf
-        img.image_orig_sha1 = sha1
-        img.image_jpeg = suf_jpeg
-        img.save()
-        serializer = serializers.ImageSerializer(img)
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+        try:
+            with transaction.atomic():
+                img = get_object_or_404(models.Image, id=kwargs['pk'])
+                suf, sha1, suf_jpeg = lungmap_utils.get_image_from_s3(img.s3key)
+                img.image_orig = suf
+                img.image_orig_sha1 = sha1
+                img.image_jpeg = suf_jpeg
+                img.save()
+                serializer = serializers.ImageSerializer(img)
+                return Response(
+                    serializer.data,
+                    status=status.HTTP_201_CREATED
+                )
+        except Exception as e:  # catch any exception to rollback changes
+            if hasattr(e, 'messages'):
+                return Response(data={'detail': e.messages}, status=400)
+            return Response(data={'detail': e.message}, status=400)
+
 
 @api_view(['GET'])
 def get_image_jpeg(request, pk):
