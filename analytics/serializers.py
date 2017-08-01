d1c6e8182f30b155b06268d5246ec35e5d079f7f
nn31 <nn31@nn31s-MacBook-Pro.local>
2017-07-31 21:21:30 -0400

    removing ids from serializer as causing issues during POST /api/subregions

--- analytics/serializers.py
+++ analytics/serializers.py
@@ -52,20 +52,18 @@ class LungmapImageSerializer(serializers.ModelSerializer):
 
 
 class PointsSerializer(serializers.ModelSerializer):
-    points_id = serializers.CharField(source='id')
 
     class Meta:
         model = models.Points
-        fields = ('points_id', 'x', 'y', 'order')
+        fields = ('x', 'y', 'order')
 
 
 class SubregionSerializer(serializers.ModelSerializer):
-    subregion_id = serializers.CharField(source='id')
     points = PointsSerializer(many=True)
 
     class Meta:
         model = models.Subregion
-        fields = ["subregion_id", "image", "anatomy", "points"]
+        fields = ["image", "anatomy", "points"]
 
     def create(self, validated_data):
         points_data = validated_data.pop('points')
