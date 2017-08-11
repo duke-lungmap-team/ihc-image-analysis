54d715e1b63400b5f03a5750220fa265f60794eb
Scott White <whitews@gmail.com>
2017-08-11 15:43:29 -0400

    PEP8 fixes

--- preload_analytics_models.py
+++ preload_analytics_models.py
@@ -9,7 +9,8 @@ from analytics import models
 image_sets = lungmap_utils.get_image_set_candidates()
 
 for key, value in image_sets.items():
-    print('loading imageset ', key)
+    print('loading image set ', key)
+
     image_set = models.ImageSet.objects.get_or_create(
         image_set_name=key,
         magnification=value['magnification'],
@@ -21,7 +22,8 @@ for key, value in image_sets.items():
         experiment, experiment_create = models.Experiment.objects.get_or_create(
             experiment_id=image['image_id'].split('_')[0]
         )
-        imageobject = models.Image.objects.get_or_create(
+
+        image_object = models.Image.objects.get_or_create(
             s3key=image['s3key'],
             image_name=image['image_name'],
             image_id=image['image_id'],
@@ -31,41 +33,44 @@ for key, value in image_sets.items():
             experiment_id=experiment.experiment_id
         )
 
-    for probe in value['probes']:
-        probeobject, probeobject_create = models.Probe.objects.get_or_create(
-            label=probe['probe_label']
+    for p in value['probes']:
+        probe_object, probe_object_create = models.Probe.objects.get_or_create(
+            label=p['probe_label']
         )
-        imagesetprobemap = models.ImageSetProbeMap.objects.get_or_create(
-            color=probe['color'],
-            probe=probeobject,
+
+        image_set_probe_map = models.ImageSetProbeMap.objects.get_or_create(
+            color=p['color'],
+            probe=probe_object,
             image_set=image_set[0]
         )
 
         for exp in value['experiments']:
-            experimentobject = models.Experiment.objects.get(
+            experiment_object = models.Experiment.objects.get(
                 experiment_id=exp
             )
-            experimentprobemap = models.ExperimentProbeMap.objects.create(
-                color=probe['color'],
-                experiment_id=experimentobject,
-                probe=probeobject,
+
+            experiment_probe_map = models.ExperimentProbeMap.objects.create(
+                color=p['color'],
+                experiment_id=experiment_object,
+                probe=probe_object,
             )
 
-#need to fill in with actual data from ontology
+
+# need to fill in with actual data from ontology
 def add_probe_anatomy(probe, class_name):
     """
     Add a probe/classification combo, this can be either cell or stucture as identified
     by the class_type
     :param probe: str: a probe name
     :param class_name: str: a classficiation name
-    :param class_type: str: either 'cell' or 'structure'
     :return:
     """
     probe_instance = models.Probe.objects.get(label=probe)
     anatomy = models.Anatomy.objects.create(name=class_name)
-    pc_mapping = models.AnatomyProbeMap.objects.create(
-        probe = probe_instance,
-        anatomy = anatomy
+
+    models.AnatomyProbeMap.objects.create(
+        probe=probe_instance,
+        anatomy=anatomy
     )
 
 add_probe_anatomy('Sox2', 'bronchiolar_epithelial_cell')
