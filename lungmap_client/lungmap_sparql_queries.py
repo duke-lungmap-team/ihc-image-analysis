b9c51de9d8a346d76742d8480c0f86a3a11bbdbb
Scott White <whitews@gmail.com>
2017-09-29 12:33:33 -0400

    update SPARQL queries due to LungMap site changes (breaks a few things)

--- lungmap_client/lungmap_sparql_queries.py
+++ lungmap_client/lungmap_sparql_queries.py
@@ -1,163 +1,84 @@
 # https://docs.google.com/document/d/10qtrZeCFeGPZt5r6Z_TKWl0ihXBzNysp8nhOpbgrl1o/edit
 
 GET_BASIC_EXPERIMENTS = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX hont: <http://ontology.lungmap.net/ontologies/human_anatomy#>
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb:<http://www.lungmap.net/ontologies/database#>
 SELECT ?experiment_id ?species ?stage_label
 WHERE {
-    ?experiment_id lm:is_experiment_type/rdfs:label "Immunofluorescence-Confocal" .
-    ?experiment_id lm:in_organism ?tax_id .
+    VALUES ?exp_type_id { lm:LMXT0000000003 } .
+    ?experiment_id lmdb:is_experiment_type ?exp_type_id .
+    ?experiment_id lmdb:in_organism ?tax_id .
     ?tax_id rdfs:label ?species . 
-    ?experiment_id lm:uses_sample ?sample_id . 
-    ?sample_id lm:in_stage ?stage .
+    ?experiment_id lmdb:uses_sample ?sample_id . 
+    ?sample_id lmdb:in_stage ?stage .
     ?stage rdfs:label ?stage_label .
 }
 """
 
 ALL_EXPERIMENTS_WITH_IMAGE = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-SELECT DISTINCT ?experiment, ?experiment_type_label
-FROM  <http://data.lungmap.net/lungmap_data>
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb:<http://www.lungmap.net/ontologies/database#>
+SELECT DISTINCT ?experiment
 WHERE {
-    ?image lm:part_of_experiment ?experiment .
-    ?experiment lm:is_experiment_type/rdfs:label ?experiment_type_label .
-    ?experiment lm:is_experiment_type/rdfs:label "Immunofluorescence-Confocal" .
-    ?image lm:display_url ?img_file .
-    ?image lm:magnification ?magnification .
-    ?image lm:x_scaling ?x_scaling .
-    ?image lm:y_scaling ?y_scaling .
-    ?experiment lm:file_name ?path .
+    VALUES (?exp_type ?image_type) { (lm:LMXT0000000003 lmdb:expression_image) } .
+    ?experiment lmdb:is_experiment_type ?exp_type .
+    ?image lmdb:part_of_experiment ?experiment .
+    ?image rdf:type ?image_type
 } ORDER BY ASC(?experiment)
 """
 
 GET_IMAGES_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?experiment ?image ?path ?img_file ?magnification ?x_scaling ?y_scaling
-FROM  <http://data.lungmap.net/lungmap_data>
-FROM  <http://data.lungmap.net/sequence_mappings>
-FROM  <http://data.lungmap.net/lungmap_ontology>
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
+SELECT DISTINCT ?experiment ?image ?path ?dir ?magnification ?x_scaling ?y_scaling
 WHERE {
-    VALUES ?experiment { owl2:EXPERIMENT_PLACEHOLDER } .
-    ?image lm:part_of_experiment ?experiment .
-    ?image lm:display_url ?img_file .
-    ?image lm:magnification ?magnification .
-    ?image lm:x_scaling ?x_scaling .
-    ?image lm:y_scaling ?y_scaling .
-    ?experiment lm:file_name ?path .
+    VALUES ?experiment { lm:EXPERIMENT_PLACEHOLDER } .
+    ?image lmdb:part_of_experiment ?experiment .
+    ?image lmdb:directory ?dir .
+    ?image lmdb:magnification ?magnification .
+    ?image lmdb:x_scaling ?x_scaling .
+    ?image lmdb:y_scaling ?y_scaling .
+    ?experiment lmdb:s3_path ?path .
 } ORDER BY ASC(xsd:integer(REPLACE(str(?magnification), 'X', '')))
 """
 
-GET_ANATOMY_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?experiment ?term ?term_label
-FROM  <http://data.lungmap.net/lungmap_data>
-FROM  <http://data.lungmap.net/sequence_mappings>
-FROM  <http://data.lungmap.net/lungmap_ontology>
-WHERE {
-    VALUES ?experiment { owl2:EXPERIMENT_PLACEHOLDER } .
-    ?experiment lm:has_condition ?term .
-    ?term rdfs:label ?term_label
-}"""
-
 GET_SAMPLE_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX hont: <http://ontology.lungmap.net/ontologies/human_anatomy#>
-SELECT ?sample_id ?tax_id ?organism_label ?local_id ?age ?age_label ?age_group ?age_group_label ?weight ?sex ?race ?cause_of_death ?health_status ?strain ?genotype ?crown_rump_length ?harvest_date
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
+SELECT ?sample_id ?organism_label ?local_id ?age_label
 WHERE {
-    VALUES ?experiment_id { owl2:EXPERIMENT_PLACEHOLDER }
-    ?experiment_id lm:uses_sample ?sample_id .
-    ?sample_id lm:in_organism ?tax_id .
+    VALUES ?experiment_id { lm:EXPERIMENT_PLACEHOLDER }
+    ?experiment_id lmdb:uses_sample ?sample_id .
+    ?sample_id lmdb:in_organism ?tax_id .
     ?tax_id rdfs:label ?organism_label .
-    ?sample_id lm:local_id ?local_id .
-    ?sample_id lm:in_stage ?age .
-    OPTIONAL { ?age rdfs:label ?age_label }
-    FILTER NOT EXISTS { ?age rdfs:subClassOf hont:LMHA0000000648 }
-    OPTIONAL {
-       ?sample_id lm:in_stage ?age_group .
-       ?age_group rdfs:label ?age_group_label .
-       ?age_group rdfs:subClassOf hont:LMHA0000000648
-    }
-    ?sample_id lm:weight ?weight .
-    ?sample_id lm:gender ?sex .
-    OPTIONAL {?sample_id lm:race ?race }
-    OPTIONAL {?sample_id lm:cause_of_death ?cause_of_death }
-    OPTIONAL {?sample_id lm:health_status ?health_status }
-    OPTIONAL {?sample_id lm:in_strain ?strain }
-    OPTIONAL {?sample_id lm:genotype ?genotype }
-    OPTIONAL {?sample_id lm:crown_rump_length ?crown_rump_length }
-    OPTIONAL {?sample_id lm:harvest_date ?harvest_date }
-}"""
+    ?sample_id lmdb:local_id ?local_id .
+    ?sample_id lmdb:in_stage ?age .
+    ?age rdfs:label ?age_label
+}
+"""
 
 GET_PROBE_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?experiment_id ?probe_id ?probe_label ?color (GROUP_CONCAT(DISTINCT CONCAT(?gene_id, ';', ?symbol),'|') as ?target_molecules) (GROUP_CONCAT(DISTINCT CONCAT(?anatomy,';',?anatomy_label),'|') AS ?target_conditions)
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
+SELECT DISTINCT ?experiment_id ?probe_id ?probe_label ?color
 WHERE {
-    VALUES ?experiment_id { owl2:EXPERIMENT_PLACEHOLDER } .
-    ?experiment_id lm:in_organism ?tax_id .
-    ?image lm:part_of_experiment ?experiment_id .
-    ?image lm:has_probe ?probe_id .
-    ?probe_color lm:maps_to ?probe_id .
-    ?probe_color lm:maps_to ?image .
-    ?probe_color lm:color ?color .
-    OPTIONAL { 
-        ?probe_id lm:maps_to ?resource .
-        ?resource lm:maps_to{0,1} ?gene_id .
-        ?gene_id lm:id_type owl:Gene_ID .
-        ?gene_id rdfs:label ?symbol .
-        ?gene_id lm:in_organism ?tax_id .
-    }
-    OPTIONAL { ?probe_id rdfs:label ?probe_label }
-    OPTIONAL { 
-        ?probe_id lm:probe_target_condition ?anatomy .
-        ?anatomy rdfs:label ?anatomy_label
-    }
+    VALUES ?experiment_id { lm:EXPERIMENT_PLACEHOLDER } .
+    ?experiment_id lmdb:in_organism ?tax_id .
+    ?image lmdb:part_of_experiment ?experiment_id .
+    ?image lmdb:has_probe ?probe_id .
+    ?probe_color lmdb:maps_to ?probe_id .
+    ?probe_color lmdb:maps_to ?image .
+    ?probe_color lmdb:color ?color .
+    ?probe_id rdfs:label ?probe_label
 }
 """
 
 GET_EXPERIMENT_TYPE_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?experiment ?experiment_type_label ?release_date ?description ?platform
-FROM  <http://data.lungmap.net/lungmap_data>
-FROM  <http://data.lungmap.net/lungmap_ontology>
+PREFIX lm: <http://www.lungmap.net/ontologies/data#>
+PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
+SELECT DISTINCT ?experiment ?experiment_type_label
 WHERE {
-    VALUES ?experiment { owl2:EXPERIMENT_PLACEHOLDER } .
-    ?experiment lm:is_experiment_type/rdfs:label ?experiment_type_label .
-    OPTIONAL { ?experiment lm:creation_date|(^lm:part_of_experiment/lm:creation_date) ?release_date }
-    OPTIONAL { ?experiment rdfs:comment ?description }
-    OPTIONAL { ?experiment lm:platform|(^lm:part_of_experiment/lm:platform) ?platform }
-}"""
-
-GET_RESEARCHER_BY_EXPERIMENT = """
-PREFIX lm: <http://ontology.lungmap.net/ontologies/expression_ontology#>
-PREFIX mont: <http://ontology.lungmap.net/ontologies/mouse_anatomy#>
-PREFIX owl: <http://www.w3.org/2002/07/>
-PREFIX owl2: <http://www.w3.org/2002/07/owl#>
-SELECT DISTINCT ?experiment ?researcher_label ?site_label
-FROM  <http://data.lungmap.net/lungmap_data>
-FROM  <http://data.lungmap.net/lungmap_ontology>
-WHERE {
-    VALUES ?experiment { owl2:EXPERIMENT_PLACEHOLDER } .
-    ?researcher a lm:researcher .
-    ?experiment lm:part_of ?researcher .
-    ?site a lm:site .
-    ?researcher lm:part_of ?site .
-    ?researcher rdfs:label ?researcher_label .
-    ?site rdfs:label ?site_label .
-}"""
+    VALUES ?experiment { lm:EXPERIMENT_PLACEHOLDER } .
+    ?experiment lmdb:is_experiment_type/rdfs:label ?experiment_type_label .
+}
+"""
