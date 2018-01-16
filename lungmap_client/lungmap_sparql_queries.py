143646ea66b344c1b1ddd3cb7e365b7db0048f04
Scott White <whitews@gmail.com>
2018-01-16 14:39:22 -0500

    remove unused queries

--- lungmap_client/lungmap_sparql_queries.py
+++ lungmap_client/lungmap_sparql_queries.py
@@ -15,18 +15,6 @@ WHERE {
 }
 """
 
-ALL_EXPERIMENTS_WITH_IMAGE = """
-PREFIX lm: <http://www.lungmap.net/ontologies/data#>
-PREFIX lmdb:<http://www.lungmap.net/ontologies/database#>
-SELECT DISTINCT ?experiment
-WHERE {
-    VALUES (?exp_type ?image_type) { (lm:LMXT0000000003 lmdb:expression_image) } .
-    ?experiment lmdb:is_experiment_type ?exp_type .
-    ?image lmdb:part_of_experiment ?experiment .
-    ?image rdf:type ?image_type
-} ORDER BY ASC(?experiment)
-"""
-
 GET_IMAGES_BY_EXPERIMENT = """
 PREFIX lm: <http://www.lungmap.net/ontologies/data#>
 PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
@@ -43,20 +31,6 @@ WHERE {
 } ORDER BY ASC(xsd:integer(REPLACE(str(?magnification), 'X', '')))
 """
 
-GET_SAMPLE_BY_EXPERIMENT = """
-PREFIX lm: <http://www.lungmap.net/ontologies/data#>
-PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
-SELECT ?sample_id ?organism_label ?local_id ?age_label
-WHERE {
-    VALUES ?experiment_id { lm:EXPERIMENT_PLACEHOLDER }
-    ?experiment_id lmdb:uses_sample ?sample_id .
-    ?sample_id lmdb:in_organism ?tax_id .
-    ?tax_id rdfs:label ?organism_label .
-    ?sample_id lmdb:local_id ?local_id .
-    ?sample_id lmdb:in_stage ?age .
-    ?age rdfs:label ?age_label
-}
-"""
 
 GET_PROBE_BY_EXPERIMENT = """
 PREFIX lm: <http://www.lungmap.net/ontologies/data#>
@@ -71,15 +45,5 @@ WHERE {
     ?probe_color lmdb:maps_to ?image .
     ?probe_color lmdb:color ?color .
     ?probe_id rdfs:label ?probe_label
-}
-"""
-
-GET_EXPERIMENT_TYPE_BY_EXPERIMENT = """
-PREFIX lm: <http://www.lungmap.net/ontologies/data#>
-PREFIX lmdb: <http://www.lungmap.net/ontologies/database#>
-SELECT DISTINCT ?experiment ?experiment_type_label
-WHERE {
-    VALUES ?experiment { lm:EXPERIMENT_PLACEHOLDER } .
-    ?experiment lmdb:is_experiment_type/rdfs:label ?experiment_type_label .
 }
 """
